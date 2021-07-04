4a4bc499ff23f99cd137f73a5805d55819d5ca0d
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2021-07-04 17:58:13 +0200

    refactored writeCmdIDFile into writeCmdState

--- cmdFileUtils.go
+++ cmdFileUtils.go
@@ -23,6 +23,7 @@ const (
 	EXECUTING
 	DONE
 	NO_PARENT
+	NO_EXIT_YET
 )
 
 // checks to see if we are the parent command or if we should wait for some parent to stop executing first
@@ -51,17 +52,25 @@ func getCommandParentState() (int, CommandConfig) {
 }
 
 // Writes the cmdID.json file
-func writeCmdIDFile(state int) {
-	jsonContents := CommandConfig{
-		Cmd:          cmd,
-		CmdID:        cmdID,
-		CmdStart:     time.Now().Unix(),
-		CommandState: state,
-	}
-	content, _ := json.Marshal(jsonContents)
-	err := ioutil.WriteFile(filepath.Join(CFG_DIR_PATH, cmdID+".json"), content, 0644)
-	if err != nil {
-		logger.Fatal(err)
+// if it exists do only modify the state
+// and or append exit codes
+func writeCmdState(CommandState int, ExitCode int) {
+	cmdIDFile := filepath.Join(CFG_DIR_PATH, cmdID+".json")
+	if fileExists(cmdIDFile) {
+		jsonContents := readCmdIDFile(cmdIDFile)
+		jsonContents.CommandState = CommandState
+		jsonContents.ExitCode = ExitCode
+		writeCmdIDFileRaw(cmdIDFile, jsonContents)
+
+	} else {
+		jsonContents := CommandConfig{
+			Cmd:          cmd,
+			CmdID:        cmdID,
+			CmdStart:     time.Now().Unix(),
+			CommandState: CommandState,
+			ExitCode:     ExitCode,
+		}
+		writeCmdIDFileRaw(cmdIDFile, jsonContents)
 	}
 }
 
@@ -81,6 +90,33 @@ func listCmdIDFiles() []os.FileInfo {
 		}
 	}
 	return files
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+func readCmdIDFile(cmdIDFile string) CommandConfig {
+	var jsonContents CommandConfig
+	dat, err := ioutil.ReadFile(cmdIDFile)
+	if err != nil {
+		logger.Fatal(err)
+		return CommandConfig{}
+	}
+
+	json.Unmarshal([]byte(dat), &jsonContents)
+	return jsonContents
+}
+
+func writeCmdIDFileRaw(cmdIDFile string, jsonContents CommandConfig) {
+	content, _ := json.Marshal(jsonContents)
+
+	err := ioutil.WriteFile(cmdIDFile, content, 0644)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
 
 func cleanUP() {
